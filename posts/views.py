64f7c1892e657baf922918c8e82734d4cc315e42
dk445 <kartik.dambre@gmail.com>
2019-08-18 00:16:35 +0530

    Gitacora saved 1 changed files

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ from posts.models import UserPost
 
 def makepost(request):
     curent_user = request.user
-    if curent_user.is_authenticated():
+    if curent_user.is_authenticated:
         return redirect('/api/signin/')
     else:
         desc = request.POST['desc']     
@@ -19,7 +19,7 @@ def makepost(request):
 
 def displaypost(request):
     curent_user = request.user
-    if curent_user.is_authenticated():
+    if curent_user.is_authenticated:
         return redirect('/api/signin/')
     else:
         posts = UserPost()
