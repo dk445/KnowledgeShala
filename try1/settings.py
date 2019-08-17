3d07411dda93a14aa160fca8e3f427d9ed2169e8
dk445 <kartik.dambre@gmail.com>
2019-08-17 23:59:09 +0530

    Gitacora saved 1 changed files

--- try1/settings.py
+++ try1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!&lpe0_!qsc0l71p%nfec!h0qi@l%0ju-#7_94eaq8_e(vsr7#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
