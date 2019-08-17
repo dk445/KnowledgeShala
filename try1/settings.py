8303b3ffda3722e07b7cb400af388a8ad8c12e71
dk445 <kartik.dambre@gmail.com>
2019-08-18 00:00:26 +0530

    Gitacora saved 1 changed files

--- try1/settings.py
+++ try1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!&lpe0_!qsc0l71p%nfec!h0qi@l%0ju-#7_94eaq8_e(vsr7#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
