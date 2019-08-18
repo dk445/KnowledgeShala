1fb2e38c30922c82c8129650306a797592777bd7
dk445 <kartik.dambre@gmail.com>
2019-08-18 19:06:23 +0530

    Gitacora saved 1 changed files

--- posts/views.py
+++ posts/views.py
@@ -6,22 +6,24 @@ from posts.models import UserPost
 # Create your views here.
 
 def makepost(request):
-    curent_user = request.user
+    '''curent_user = request.user
     if not curent_user.is_authenticated:
         return redirect('/api/signin/')
-    else:
-        desc = request.POST['desc']     
-        owner = curent_user.email
-        created = timezone.localtime()
-        UserPost.objects.create(owner=owner , description = desc , createdon = created)    
-        print('posted')
-        return redirect('/feed')
+    else:'''
+        
+    desc = request.POST['desc']     
+    owner = curent_user.email
+    created = timezone.localtime()
+    UserPost.objects.create(owner=owner , description = desc , createdon = created)    
+    print('posted')
+    return redirect('/feed')
 
 def displaypost(request):
-    curent_user = request.user
+    '''curent_user = request.user
     if request.user.is_authenticated:
         return redirect('/api/signin/')
-    else:
-        posts = UserPost()
-        posts = UserPost.objects.all().order_by('createdon').reverse()
-        return render(request,'feed.html',{'posts':posts})
+    else:'''
+
+    posts = UserPost()
+    posts = UserPost.objects.all().order_by('createdon').reverse()
+    return render(request,'feed.html',{'posts':posts})
