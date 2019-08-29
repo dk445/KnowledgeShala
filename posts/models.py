4398644d6b18cacc74e239d791073e200d736268
dk445 <kartik.dambre@gmail.com>
2019-08-29 23:37:45 +0530

    Gitacora saved 1 changed files

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from signup.models import UserData
 
 # Create your models here.
 
 class UserPost(models.Model):
     postid = models.AutoField(primary_key=True)
-    owner = models.EmailField()
+    owner = models.ForeignKey(UserData,on_delete= models.CASCADE,related_name="post_owner")
     description = models.TextField(blank=True)
     createdon = models.DateTimeField()
     
