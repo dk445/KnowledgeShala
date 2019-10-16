270e90ee98e5a44ccc8267c1a317fa79ef52c52d
dk445 <kartik.dambre@gmail.com>
2019-10-16 22:16:52 +0530

    Gitacora saved 1 changed files

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/feed',views.displaypost),
+    path('feed/',views.displaypost),
     path('feed/post/',views.makepost)
 ]
                                                                         
