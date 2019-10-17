aa501a9bbac17ceb28779d2ba39f5d205acc94f4
dk445 <kartik.dambre@gmail.com>
2019-10-17 18:31:00 +0530

    Gitacora saved 1 changed files

--- college/views.py
+++ college/views.py
@@ -3,19 +3,23 @@ from signup.models import CollegeData
 
 # Create your views here.
 def addcollege(request):
-    if request.method=='POST':
-        clgname = request.POST['collegename']
-        clgid = request.POST['collegeid']
-        city = request.POST['city']
-        email = request.POST['email']
-        password = request.POST['pwd']
-
-        if password=='@123abc':
-            CollegeData.objects.create(clgName = clgname , clgid = clgid , city = city , email=email)
-            print('clg added')
-            return render(request,'college.html',{'message':'Added successfully'})
-        else:
-            print('wrong pwd')
-            return render(request,'college.html',{'message':'wrong password'})
+    if request.user.is_authenticated:
+        alert('sign out first')
+        return redirect('/api/signin/')
     else:
-        return render(request,'college.html')
+        if request.method=='POST':
+            clgname = request.POST['collegename']
+            clgid = request.POST['collegeid']
+            city = request.POST['city']
+            email = request.POST['email']
+            password = request.POST['pwd']
+
+            if password=='@123abc':
+                CollegeData.objects.create(clgName = clgname , clgid = clgid , city = city , email=email)
+                print('clg added')
+                return render(request,'college.html',{'message':'Added successfully'})
+            else:
+                print('wrong pwd')
+                return render(request,'college.html',{'message':'wrong password'})
+        else:
+            return render(request,'college.html')
