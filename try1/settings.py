064e7be381c0b524450ef95cb20716b5572e5b88
dk445 <kartik.dambre@gmail.com>
2019-10-17 20:23:17 +0530

    Gitacora saved 1 changed files

--- try1/settings.py
+++ try1/settings.py
@@ -141,8 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 AUTH_USER_MODEL =  'signup.UserData'
 
 # EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
-EMAIL_HOST = ‘smtp.gmail.com’
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ‘kartik.dambre@gmail.com’
-EMAIL_HOST_PASSWORD = ‘Vadodara445’
+EMAIL_HOST_USER = 'kartik.dambre@gmail.com'
+EMAIL_HOST_PASSWORD = 'Vadodara445'
