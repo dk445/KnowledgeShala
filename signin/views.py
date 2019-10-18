7213c2587705d0c906a3c2a42b6cfe19ed9b54b9
dk445 <kartik.dambre@gmail.com>
2019-10-18 12:56:37 +0530

    Gitacora saved 1 changed files

--- signin/views.py
+++ signin/views.py
@@ -25,7 +25,7 @@ def collegesignin(request):
             print(college.email)
             if(check_password(password,college.password)):
                 return (college)
-                return render(request,'collegeHome.html',{'college': college}) 
+               #return render(request,'collegeHome.html',{'college': college}) 
             else:
                 return render(request,'collegepage.html')
 def signinPage(request):
