e47b2e2d1fe2d285b70f100064bbdab4708347c2
dk445 <kartik.dambre@gmail.com>
2019-10-18 21:53:55 +0530

    Gitacora saved 1 changed files

--- college/views.py
+++ college/views.py
@@ -8,8 +8,9 @@ from signup.models import UserData,CollegeData,DepartmentData,RoleData
 
 # Create your views here.
 
-def collegeFeed(request,email):
-    college = CollegeData.objects.get(email=email)
+def collegeFeed(request,password):
+
+    college = CollegeData.objects.get(password=password)
     print(college.email)
     requests = UserData.objects.filter(isVerified = 'No') & UserData.objects.filter(clgid_id=college.clgid)
     collegeusers = UserData.objects.filter(clgid_id=college.clgid)
