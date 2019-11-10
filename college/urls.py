c50aa9d2900ecb67c6830388479c8daf866828d9
dk445 <kartik.dambre@gmail.com>
2019-11-10 12:28:36 +0530

    Gitacora saved 1 changed files

--- college/urls.py
+++ college/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('add/',views.addcollege),
-    path('college/<password>',views.collegeFeed),
-    path('college/<password>/requests',views.requeststocollege),
+    path('college/<id>',views.collegeFeed),
+    path('college/<id>/requests',views.requeststocollege),
 ]
