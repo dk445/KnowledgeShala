39a46f1a73bf9f8257c8a2f2fb355388e5685151
dk445 <kartik.dambre@gmail.com>
2019-12-02 19:15:54 +0530

    Gitacora saved 3 changed files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         ]
     },
     plugins: [new HtmlWebpackPlugin({
-        template: '/index.html'
+        template: './src/index.html'
     })],
     devServer: {
         historyApiFallback: true
