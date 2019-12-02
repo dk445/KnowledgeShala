b46fd6e52dcde02a164cda3ab2470a0c57f4afe4
dk445 <kartik.dambre@gmail.com>
2019-12-02 17:22:35 +0530

    Gitacora saved 3 changed files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
+    context: path.join(__dirname, "src"),
     entry: '/index.js',
     mode: 'development',
     resolve: {
