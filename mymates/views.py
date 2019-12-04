a0072081322540a6cd36b9afccec329db33f9638
dk445 <kartik.dambre@gmail.com>
2019-12-05 00:21:31 +0530

    Gitacora saved 13 changed files

--- mymates/views.py
+++ mymates/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from requests.models import  Relation
 from signup.models import UserData
 from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
@@ -10,5 +11,6 @@ def displaymates(request):
     loggedinuser = request.user.email
 
     mates = Relation.objects.filter(user_id=loggedinuser)
+    result = serializers.serialize('json',mates)
 
-    return render(request,'mates.html',{'result':mates})
+    return HttpResponse(result)
