ca083e7b6e969ed25925d118e4b8c068d421c6e1
dk445 <kartik.dambre@gmail.com>
2019-12-05 01:03:05 +0530

    Gitacora saved 13 changed files

--- account/views.py
+++ account/views.py
@@ -11,24 +11,26 @@ def profile(request,requestedemail):
         return redirect('/api/signin/')
     else:
         result=[]
-       # data = UserData.objects.get(email = requestedemail)
-       # data_serializer = serializers.serialize('json',data)   
-       # result.append(data_serializer)
+        try:
+            data = UserData.objects.get(email = requestedemail)
+            data_serializer = serializers.serialize('json',data)   
+            result.append(data_serializer)
 
-        posts = UserPost.objects.filter(owner = requestedemail)
-        posts_serializer = serializers.serialize('json',posts)  
-        result.append(posts_serializer) 
+            posts = UserPost.objects.filter(owner = requestedemail)
+            posts_serializer = serializers.serialize('json',posts)  
+            result.append(posts_serializer) 
 
-        mateList =  Relation.objects.filter(user = requestedemail)
-        mateList_serializer = serializers.serialize('json',mateList) 
-        result.append(mateList_serializer)  
+            mateList =  Relation.objects.filter(user = requestedemail)
+            mateList_serializer = serializers.serialize('json',mateList) 
+            result.append(mateList_serializer)  
 
-        if(request.user.email == requestedemail):
-            request_status = None
-        else:
-            request_status = (Requests.objects.get(requesting = request.user.email , requested = requestedemail)).statusid
+            if(request.user.email == requestedemail):
+                request_status = None
+            else:
+                request_status = (Requests.objects.get(requesting = request.user.email , requested = requestedemail)).statusid
 
-        
+        except:
+            return HttpResponse("error")    
         #print(request_status)
         #print(mateList[0].mate.name)
         #print(data.name)
