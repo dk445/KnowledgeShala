d41182713b48e6c1cbf271acef728b04300963ec
dk445 <kartik.dambre@gmail.com>
2019-12-05 01:09:41 +0530

    Gitacora saved 13 changed files

--- account/views.py
+++ account/views.py
@@ -11,8 +11,8 @@ def profile(request,requestedemail):
         return redirect('/api/signin/')
     else:
         result=[]
-        
-        data = UserData.objects.get(email = requestedemail)
+
+        data = UserData.objects.get(email = requestedemail).values()
         data_serializer = serializers.serialize('json',data)   
         result.append(data_serializer)
 
