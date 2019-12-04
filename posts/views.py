91d9b7f51081c530d8e1154d79fe48e4eed4b1fc
dk445 <kartik.dambre@gmail.com>
2019-12-04 22:49:48 +0530

    Gitacora saved 13 changed files

--- posts/views.py
+++ posts/views.py
@@ -38,6 +38,6 @@ def displaypost(request):
             if(relation.mate.deptid == ownerdept):   #post of mates with same deptid
                 filteredpost.append(posts)
         
-        print(JsonResponse(filteredpost))
+        return JsonResponse(filteredpost)
        # print(filteredpost)
       #  return render(request,'feed.html',{'mateposts':filteredpost})
