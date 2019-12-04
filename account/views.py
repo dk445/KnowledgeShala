a44408f6e078832e864e2f838811ee53b158ae7b
dk445 <kartik.dambre@gmail.com>
2019-12-05 00:53:48 +0530

    Gitacora saved 13 changed files

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect,HttpResponse
 from signup.models import UserData ,CollegeData , DepartmentData , RoleData
 from  posts.models import UserPost
 from requests.models import Relation , Requests
+from django.core import serializers
 
 # Create your views here.
 
@@ -9,21 +10,31 @@ def profile(request,requestedemail):
     if not request.user.is_authenticated:
         return redirect('/api/signin/')
     else:
-        data = UserData.objects.get(email = requestedemail)     
-        posts = UserPost.objects.filter(owner = requestedemail) 
+        result=[]
+        data = UserData.objects.get(email = requestedemail)
+        data_serializer = serializers.serialize('json',data)   
+        result.append(data_serializer)
+
+        posts = UserPost.objects.filter(owner = requestedemail)
+        posts_serializer = serializers.serialize('json',posts)  
+        result.append(posts_serializer) 
+
         mateList =  Relation.objects.filter(user = requestedemail)
+        mateList_serializer = serializers.serialize('json',mateList) 
+        result.append(mateList_serializer)  
+
         if(request.user.email == requestedemail):
             request_status = None
         else:
             request_status = (Requests.objects.get(requesting = request.user.email , requested = requestedemail)).statusid
 
         
-        print(request_status)
-        print(mateList[0].mate.name)
-        print(data.name)
-        print(data.isVerified)
-        print(data.roleid.rolename)
-        print(data.deptid.deptname)
-        print(posts)
+        #print(request_status)
+        #print(mateList[0].mate.name)
+        #print(data.name)
+        #print(data.isVerified)
+        #print(data.roleid.rolename)
+        #print(data.deptid.deptname)
+        #print(posts)
 
-        return render(request,'account.html',{'accountinfo':data ,'userpost':posts,'matelist':mateList,'relation':request_status})
+        return HttpResponse(result)
