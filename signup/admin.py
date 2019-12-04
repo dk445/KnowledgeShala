d09f9f6178939443aba0d7b5efcc4287185e236b
dk445 <kartik.dambre@gmail.com>
2019-12-04 21:24:11 +0530

    Gitacora saved 14 changed files

--- signup/admin.py
+++ signup/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import UserData,CollegeData,DepartmentData,RoleData
 # Register your models here.
 
- admin.site.register(UserData)
- admin.site.register(CollegeData)
- admin.site.register(DepartmentData)
- admin.site.register(RoleData)
+admin.site.register(UserData)
+admin.site.register(CollegeData)
+admin.site.register(DepartmentData)
+admin.site.register(RoleData)
