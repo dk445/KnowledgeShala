e32749bdb77115a16ffc113735d5cbd5b758312f
dk445 <kartik.dambre@gmail.com>
2019-12-05 00:57:41 +0530

    Gitacora saved 13 changed files

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/account/<requestedemail>',views.profile),
+    path('account/<requestedemail>',views.profile),
 ]
                                                                         
